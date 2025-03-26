484f4f570cd3d0152b2c597c6d9e9ae51e0cb324
martin <92221161+martinbojkovski@users.noreply.github.com>
2025-03-26 15:30:24 +0100

    Project setup.

---
+++ src/main/java/com/iktproject/IktProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iktproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IktProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IktProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iktproject/IktProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IktProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
