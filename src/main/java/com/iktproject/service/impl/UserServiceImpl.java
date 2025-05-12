5dfe7190a82e9f79e3004167b1ce2fbb649a3465
martin <92221161+martinbojkovski@users.noreply.github.com>
2025-05-12 17:14:55 +0200

    Added basic security, modified userServiceImpl, added userController.

--- src/main/java/com/iktproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/iktproject/service/impl/UserServiceImpl.java
@@ -1,19 +1,29 @@
 package com.iktproject.service.impl;
 
+import com.iktproject.exception.UserAlreadyExistsException;
 import com.iktproject.model.User;
 import com.iktproject.repository.UserRepository;
 import com.iktproject.service.UserService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -33,29 +43,42 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User save(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new UserAlreadyExistsException(user.getUsername());
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setGrade_average(0.00);
         return userRepository.save(user);
     }
 
     @Override
-    public User update(Long id,User user) {
-        User userToUpdate = userRepository.findById(id).orElse(null);
-        if (userToUpdate != null) {
-//            userToUpdate.setUsername(user.getUsername());
-//            userToUpdate.setPassword(user.getPassword());
-            
-            userToUpdate.setName(user.getName());
-            userToUpdate.setSurname(user.getSurname());
-            userToUpdate.setGrade_average(user.getGrade_average());
-            return userRepository.save(userToUpdate);
-        }
-        return null;
+    public User update(Long id, User user) {
+        User userToUpdate =
+                userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        userToUpdate.setName(user.getName());
+        userToUpdate.setSurname(user.getSurname());
+        userToUpdate.setGrade_average(user.getGrade_average());
+
+        return userRepository.save(userToUpdate);
     }
 
     @Override
     public void delete(Long id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user != null) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
         userRepository.delete(user);
-        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getType().name()))
+        );
     }
 }
